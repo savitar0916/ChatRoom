1e3b0c60cd81779b37e03c314857a778a41ca593
savitar0916 <savitar0916@gmail.com>
2024-06-10 18:52:27 +0800

    feat: 🎸 新增下載功能

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ChatRoom
 go 1.21.0
 
 require (
-	github.com/gorilla/websocket v1.5.1 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.2 // indirect
+	golang.org/x/net v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,98 +1,18 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"encoding/json"
-
-	"os"
-	"os/exec"
-
-	"github.com/gorilla/websocket"
+    "log"
+    "net/http"
+    "ChatRoom/router"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
-var upgrader = websocket.Upgrader{}
-var messages []Message
-
-type Message struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+    // 使用自定义路由器
+    r := router.NewRouter()
 
-	http.HandleFunc("/ws", handleConnections)
-	http.HandleFunc("/dump", handleDump)
-
-	go handleMessages()
-
-	log.Println("HTTP server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
-
-	clients[ws] = true
-
-	for {
-		var msg Message
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			break
-		}
-		messages = append(messages, msg)
-		broadcast <- msg
-	}
-}
-
-func handleMessages() {
-	for {
-		msg := <-broadcast
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-	}
-}
-
-func handleDump(w http.ResponseWriter, r *http.Request) {
-	// Create a temporary file to store the messages
-	tmpFile, err := os.Create("chat_dump.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tmpFile.Close()
-
-	// Write messages to the temporary file
-	json.NewEncoder(tmpFile).Encode(messages)
-
-	// Execute the Python script
-	cmd := exec.Command("python3", "dump_chat.py")
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Return success message
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Chat log dumped successfully"})
+    log.Println("HTTP server started on :8000")
+    err := http.ListenAndServe(":8000", r)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
